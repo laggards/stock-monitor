cf79ec6b99dfea595da646bf88fd4df218a8beae
laggards <laggards@hotmail.com>
2018-10-18 01:51:17 +0800

    daily commit

--- app/cloud.php
+++ app/cloud.php
@@ -107,6 +107,58 @@ Cloud::define("updateRebalance", function($params, $user) {
     }
 });
 
+/*修复调仓断点*/
+Cloud::define("updateRebalance", function($params, $user) {
+    //$prev_bebalancing_id = $rebalancing->get('prev_bebalancing_id');
+    $pQuery = new Query("Portfolios");
+    $rbQuery = new Query("Rebalancing");
+    try {
+      $portfolios = $pQuery->equalTo('status', true)->find();
+      foreach ($portfolios as $portfolio) {
+        $allBalance = $rbQuery->equalTo('portfolio', $portfolio)->select("origin_id", "prev_bebalancing_id")->find();
+        foreach ($allBalance as $balance) {
+          // code...
+        }
+
+        $rbQuery->ascend("updated_at");
+        $olderRb = $rbQuery->equalTo('portfolio', $portfolio)->first();
+        $prev_bebalancing_id = $olderRb->get('prev_bebalancing_id');
+        if(!empty($prev_bebalancing_id)){
+          $rebalance = getRebalancing($prev_bebalancing_id);
+          if(!empty($rebalance)){
+
+            $rbQuery->equalTo("origin_id", $rebalance->id);
+            if($rbQuery->count() == 0){
+              $rbObj = new LeanObject("Rebalancing");
+              $rbObj->set("portfolio", $portfolio);
+              $rbObj->set("origin_id", $rebalance->id);
+              $rbObj->set("status", $rebalance->status);
+              $rbObj->set("cube_id", $rebalance->cube_id);
+              $rbObj->set("prev_bebalancing_id", $rebalance->prev_bebalancing_id);
+              $rbObj->set("category", $rebalance->category);
+              $rbObj->set("created_at", $rebalance->created_at);
+              $rbObj->set("updated_at", $rebalance->updated_at);
+              $rbObj->set("cash_value", $rebalance->cash_value);
+              $rbObj->set("cash", $rebalance->cash);
+              $rbObj->set("error_code", $rebalance->error_code == null ? 'null': $rebalance->error_code);
+              $rbObj->set("error_message", $rebalance->error_message);
+              $rbObj->set("error_status", $rebalance->error_status == null ? 'null':$rebalance->error_status);
+              $rbObj->set("holdings", $rebalance->holdings == null ? 'null':$rebalance->holdings);
+              $rbObj->set("rebalancing_histories", json_encode($rebalance->rebalancing_histories));
+              $rbObj->set("comment", $rebalance->comment);
+              $rbObj->set("diff", $rebalance->diff);
+              $rbObj->set("new_buy_count", $rebalance->new_buy_count);
+              $rbObj->save();
+            }
+          }
+        }
+      }
+    } catch (CloudException $ex) {
+        error_log('prev_bebalancing_id->'.$prev_bebalancing_id);
+        throw new FunctionError("保存 Post 对象失败: " . $ex->getMessage());
+    }
+});
+
 Cloud::afterSave("Portfolios", function($portfolio, $currentUser) {
     $last_rb_id = $portfolio->get('last_rb_id');
     try {
@@ -136,7 +188,40 @@ Cloud::afterSave("Portfolios", function($portfolio, $currentUser) {
         }
       }
     } catch (CloudException $ex) {
-        throw new FunctionError("保存 Post 对象失败: " . $ex->getMessage());
+        throw new FunctionError("保存 组合 对象失败: " . $ex->getMessage());
+    }
+});
+
+Cloud::afterUpdate("Portfolios", function($portfolio, $currentUser) {
+    $last_rb_id = $portfolio->get('last_rb_id');
+    try {
+      if($last_rb_id){
+        $rebalance = getRebalancing($last_rb_id);
+        if(!empty($rebalance)){
+          $rbObj = new LeanObject("Rebalancing");
+          $rbObj->set("portfolio", $portfolio);
+          $rbObj->set("origin_id", $rebalance->id);
+          $rbObj->set("status", $rebalance->status);
+          $rbObj->set("cube_id", $rebalance->cube_id);
+          $rbObj->set("prev_bebalancing_id", $rebalance->prev_bebalancing_id);
+          $rbObj->set("category", $rebalance->category);
+          $rbObj->set("created_at", $rebalance->created_at);
+          $rbObj->set("updated_at", $rebalance->updated_at);
+          $rbObj->set("cash_value", $rebalance->cash_value);
+          $rbObj->set("cash", $rebalance->cash);
+          $rbObj->set("error_code", $rebalance->error_code == null ? 'null': $rebalance->error_code);
+          $rbObj->set("error_message", $rebalance->error_message);
+          $rbObj->set("error_status", $rebalance->error_status == null ? 'null':$rebalance->error_status);
+          $rbObj->set("holdings", $rebalance->holdings == null ? 'null':$rebalance->holdings);
+          $rbObj->set("rebalancing_histories", json_encode($rebalance->rebalancing_histories));
+          $rbObj->set("comment", $rebalance->comment);
+          $rbObj->set("diff", $rebalance->diff);
+          $rbObj->set("new_buy_count", $rebalance->new_buy_count);
+          $rbObj->save();
+        }
+      }
+    } catch (CloudException $ex) {
+        throw new FunctionError("保存 组合 对象失败: " . $ex->getMessage());
     }
 });
 
