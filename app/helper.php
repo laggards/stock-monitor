a471f2448a7366edd5370266ac4c50623060e95b
laggards <laggards@hotmail.com>
2018-10-18 02:30:14 +0800

    daily commit

--- app/helper.php
+++ app/helper.php
@@ -51,16 +51,3 @@ function getRebalancing($last_rb_id){
     ]);
     return json_decode($res->getBody())->rebalancing;
 }
-
-function object_to_array($obj) {
-    $obj = (array)$obj;
-    foreach ($obj as $k => $v) {
-        if (gettype($v) == 'resource') {
-            return;
-        }
-        if (gettype($v) == 'object' || gettype($v) == 'array') {
-            $obj[$k] = (array)object_to_array($v);
-        }
-    }
-    return $obj;
-}
